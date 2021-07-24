d1f20834f26380bcf9753d4ae377a4b1b1d24502
Josh Medeski <josh.medeski@gmail.com>
2021-07-24 08:59:17 -0500

    fix: remove unused bin script

--- bin/aw-tmux.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat ./themes.txt | fzf-tmux -p -m --reverse --preview 'cp ./aw-themes/{}.yml ../theme.yml && wallpaper set ./aw-themes/{}.jpg && touch ../alacritty.yml && bat --color=always --style=numbers ./colors/{}.yml'
